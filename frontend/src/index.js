cec195da51cf927544ad5e027bf7284a2dd5ce23
xinoodev <errxino.soporte@gmail.com>
2024-09-03 18:15:41 +0200

    Adding the React Routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter }  from 'react-router-dom';
 import './styles/index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
