3430f95504f03273953cb4d4f6ae5f7adb0dfd9b
xinoodev <errxino.soporte@gmail.com>
2024-09-06 21:30:06 +0200

    Adding UPDATE and DELETE event controllers and routes

--- backend/server/controllers/eventController.js
+++ backend/server/controllers/eventController.js
@@ -8,9 +8,35 @@ const createEvent = async (req, res) => {
 };
 
 const getEvents = async (req, res) => {
-    const result = db.query('SELECT * FROM events WHERE calendar_id = $1', [req.params.calendar_id]);
+    const result = await db.query('SELECT * FROM events WHERE calendar_id = $1', [req.params.calendar_id]);
 
     return res.json(result.rows);
 };
 
-module.exports = { createEvent, getEvents };
+const updateEvent = async (req, res) => {
+    const { id } = req.params;
+    const { title, description, start_time, end_time } = req.body;
+    const result = await db.query(
+        'UPDATE events SET title = $1, description = $2, start_time = $3, end_time = $4 WHERE id = $5 RETURNING *', 
+        [title, description, start_time, end_time, id]
+    );
+
+    if (result.rowCount === 0) {
+        return res.status(404).json({ error: 'Event not found.' });
+    }
+
+    return res.json(result.rows[0])
+};
+
+const deleteEvent = async (req, res) => {
+    const { id } = req.params;
+    const result = await db.query('DELETE FROM events WHERE id = $1 RETURNING *', [id]);
+
+    if (result.rowCount === 0) {
+        return res.status(404).json({ error: 'Event not found.' });
+    }
+
+    return res.json({ message: 'Event successfully deleted' });
+}
+
+module.exports = { createEvent, getEvents, updateEvent, deleteEvent };
--- backend/server/routes/eventRoutes.js
+++ backend/server/routes/eventRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { createEvent, getEvents } = require('../controllers/eventController');
+const { createEvent, getEvents, updateEvent, deleteEvent } = require('../controllers/eventController');
 const router = express.Router();
 
 router.post('/events', createEvent);
 router.get('/events/:calendar_id', getEvents);
+router.put('/events/:id', updateEvent);
+router.delete('/event/:id', deleteEvent);
 
 module.exports = router;
